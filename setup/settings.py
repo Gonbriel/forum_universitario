00f5ed3730bd7de04d8a589e806a0b72db5200fd
Gonbriel <gonbriel03@gmail.com>
2024-01-21 16:21:36 -0300

    Consertando a classe chamada nos instalados

--- setup/settings.py
+++ setup/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'forum.apps.GaleriaConfig',
+    'forum.apps.ForumConfig',
 ]
 
 MIDDLEWARE = [
