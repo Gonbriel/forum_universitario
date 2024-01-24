85f189503bd428b2ddef6622f5765c78699a1327
Gonbriel <gonbriel03@gmail.com>
2024-01-24 18:59:18 -0300

    Arrumando o problema do template.

--- forum/views.py
+++ forum/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    return render(request, 'templates/index/index.html')
+    return render(request, 'index/index.html')
--- setup/settings.py
+++ setup/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'forum.apps.ForumConfig',
+    'forum',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'forum/static')
+     os.path.join(BASE_DIR, 'setup/static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
