6fd76c79ac375955dffcb3ac6404c1a428db9d60
Gonbriel <gonbriel03@gmail.com>
2024-06-09 15:50:14 -0300

    excluindo biblioteca inutilizada

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 
 def login_cadastro(request):
     return render(request, 'usuarios/login_cadastro.html')
